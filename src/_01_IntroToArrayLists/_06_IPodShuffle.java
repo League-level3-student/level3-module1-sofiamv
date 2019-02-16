a2872336932d4d1f51f5bfeef1a1cfa843b5f05f
sofiamv <league@iMac-2.attlocal.net>
2019-02-16 15:59:41 -0800

    LongChipCompetition, IPodShuffle, IntegerStack(IP)
    ujnhucujhncxcxkxcncxncxnkcxkdfkjodsujvchyoufdfddf8dfsdfujldujhubhukhudri
    ulsfjhfsfvfsojfsiofsh

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,16 +1,35 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 //Copyright The League of Amazing Programmers, 2015
-
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener{
+JPanel panel;
+JFrame frame;
+JButton button;
+ArrayList<Song>Songs = new ArrayList<Song>();
+Random r = new Random();
+int ah = r.nextInt(5);
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
+		Song song1 = new Song("song1.mp3");
+		Song song2 = new Song("song2.mp3");
+		Song song3 = new Song("song3.mp3");
+		Song song4 = new Song("song4.mp3");
+		Song song5 = new Song("song5.mp3");
+		Songs.add(song1);
+		Songs.add(song5);
+		Songs.add(song4);
+		Songs.add(song3);
+		Songs.add(song2);
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -18,10 +37,29 @@ public class _06_IPodShuffle{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		
 	}
 	
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
+		_06_IPodShuffle a = new _06_IPodShuffle();
+		a.gah();
+	}
+	public void gah() {
+		button = new JButton("Surprise me!");
+		panel = new JPanel();
+		frame = new JFrame();
+		frame.add(panel);
+		panel.add(button);
+		button.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(button)) {
+		Songs.get(ah).play();
+		System.out.println(Songs.get(ah));
+		}
 	}
 }
--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -2,46 +2,50 @@ package _02_Integer_Stack;
 
 public class IntegerStack {
 	//1. create a private array of integers
-	
+	private int ooh[];
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		ooh = new int[0];
 	}
-	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
-		
+		int ah[] = new int[ooh.length+1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		ah[ooh.length+1] = v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for(int i = 0; i < ooh.length; i++) {
+			ah[i] = ooh[i];
+		}
 		//D. set the member array equal to the new array.
-		
+		ooh = ah;
+		System.out.println();
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int b = ooh[ooh.length-1];
 		//B. create a new array that is one element smaller than the member array
-		
+		int gah[] = new int [ooh.length-1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for(int i = 0; i < ooh.length; i++) {
+			gah[i] = ooh[i];
+		}
 		//D. set the member array equal to the new array
-		
+		ooh = gah;
 		//E. return the variable you created in step A
-		return 0;
+		return b;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		ooh = new int[0];
 	}
-	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
